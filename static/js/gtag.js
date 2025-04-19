732c9cd9e363e8b60e69bcb5c4c9baeadab9a560
Tunde-cs <tundeoshodi99@gmail.com>
2025-04-19 15:04:17 -0400

    ✅ Fix autofill warnings, CSP improvements, cleaned GA tracking

---
+++ static/js/gtag.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-8PB2K8NTLM', { allow_ad_personalization_signals: true });
